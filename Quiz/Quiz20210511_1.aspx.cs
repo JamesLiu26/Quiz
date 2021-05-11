b6b76c4c836a03e5ed2a026dc35faab127b04673
晉豪 劉 <107111236@mail.oit.edu.tw>
2021-05-11 16:36:07 +0800

    Quiz2 finish

--- Quiz/Quiz20210511_1.aspx.cs
+++ Quiz/Quiz20210511_1.aspx.cs
@@ -8,6 +8,8 @@ using System.Web.UI.WebControls;
 namespace Quiz {
     public partial class Quiz20210511_1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            Response.Write("ID: "+Session["ID"]+"<br/>");
+            Response.Write("Name: "+Session["Name"]);
 
         }
     }
